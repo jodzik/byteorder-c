880696ae66c3740c0eb6c9811ec28fe436f4d77e
jodzik <jodzikk@yandex.ru>
2026-05-18 20:16:26 +0700

    feat: add zephyr interlayer

--- byteorder.h
+++ byteorder.h
@@ -3,10 +3,42 @@
 
 #include <stdint.h>
 
+#ifdef __ZEPHYR__
+
+#include <zephyr/sys/byteorder.h>
+
+static inline uint16_t u16_from_le(uint8_t const* buf) {
+    return sys_get_le16(buf);
+}
+
+static inline void u16_to_le(uint8_t* buf, uint16_t val) {
+    sys_put_le16(val, buf);
+}
+
+static inline uint32_t u32_from_le(uint8_t const* buf) {
+    return sys_get_le32(buf);
+}
+
+static inline void u32_to_le(uint8_t* buf, uint32_t val) {
+    sys_put_le32(val, buf);
+}
+
+static inline uint32_t u64_from_le(uint8_t const* buf) {
+    return sys_get_le64(buf);
+}
+
+static inline void u64_to_le(uint8_t* buf, uint32_t val) {
+    sys_put_le64(val, buf);
+}
+
+#else
+
 uint16_t u16_from_be(uint8_t const* buf);
 void u16_to_be(uint8_t* buf, uint16_t val);
 
 uint32_t u32_from_be(uint8_t const* buf);
 void u32_to_be(uint8_t* buf, uint32_t val);
+
+#endif
 
 #endif
